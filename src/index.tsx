973ae67fe533673382f85d8a15e9ce594190432f
Luna The Bat <lunathebat@proton.me>
2022-11-30 17:27:16 +0100

    nodes styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "reactflow/dist/style.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "reactflow/dist/style.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
--- src/nodes/AddNode.tsx
+++ src/nodes/AddNode.tsx
@@ -4,15 +4,15 @@ import NumberInput from "../components/input/NumberInput";
 import NodeLine from "../components/node/NodeLine";
 import NodeShell from "../components/node/NodeShell";
 
-const ConstantValueNode: FunctionComponent<NodeProps<any>> = ({ data }) => {
+const AddNode: FunctionComponent<NodeProps<any>> = ({ data }) => {
   const onChange = useCallback((evt: any) => {
     console.log(evt);
   }, []);
 
   return (
-    <NodeShell title="Constant Value">
+    <NodeShell title="Add">
       <NodeLine
-        type="output"
+        type="input"
         label="Value"
         input={
           <NumberInput
@@ -35,8 +35,9 @@ const ConstantValueNode: FunctionComponent<NodeProps<any>> = ({ data }) => {
           ></NumberInput>
         }
       ></NodeLine>
+      <NodeLine type="output" label="Value"></NodeLine>
     </NodeShell>
   );
 };
 
-export default ConstantValueNode;
+export default AddNode;
