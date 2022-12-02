d1a29b8973e3d1ece0ca22016db27b68116018d7
Luna The Bat <lunathebat@proton.me>
2022-12-02 22:48:41 +0100

    Default values and add node

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,9 +1,29 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useCallback, useMemo } from "react";
+import { useReactFlow } from "reactflow";
+import { nodeDefs } from "../engine/node";
+import { useDataStore } from "../engine/store";
 
 const Drawer: FunctionComponent<{}> = (a) => {
+  const nodes = useMemo(() => {
+    return Object.values(nodeDefs);
+  }, []);
+
+  const addNode = useDataStore((state) => state.addNode);
+
+  const handleClick = useCallback(
+    (type: string) => {
+      addNode(type);
+    },
+    [addNode]
+  );
+
   return (
-    <div className="bg-slate-800 w-60" onMouseDown={(e) => console.log(e)}>
-      Hi
+    <div className="bg-slate-800 w-60">
+      <ul>
+        {nodes.map((node) => (
+          <li onClick={() => handleClick(node.type)}>{node.type}</li>
+        ))}
+      </ul>
     </div>
   );
 };
