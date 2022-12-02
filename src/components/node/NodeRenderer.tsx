29b867a13351172ab8ae5dacff4cee6e13fdef6e
Mona Mayrhofer <mona.mayrhofer@proton.me>
2022-12-02 21:50:23 +0100

    dynamic node renderer

---
+++ src/components/node/NodeRenderer.tsx
@@ -0,0 +1,68 @@
+import { FunctionComponent, useCallback, useEffect, useMemo } from "react";
+import { NodeProps, Handle, Position, useEdges } from "reactflow";
+import NumberInput from "../input/NumberInput";
+import NodeHandleLine from "./NodeHandleLine";
+import NodeShell from "./NodeShell";
+import { NodeDef } from "../../engine/node";
+import { useInputHandleData } from "../../engine/store";
+
+export function makeNodeRenderer(
+  def: NodeDef
+): FunctionComponent<NodeProps<any>> {
+  const HeaderComponent = def.header;
+
+  return ({ id, data }) => {
+    const edges = useEdges();
+
+    const store = def.inputs.map((input) => {
+      const [value, setValue] = useInputHandleData<any>(id, input.id);
+      return {
+        value,
+        setValue,
+      };
+    });
+
+    const connections = useMemo(() => {
+      return def.inputs.map(
+        (input) =>
+          edges.findIndex(
+            (it) => it.target === id && it.targetHandle === "a"
+          ) >= 0
+      );
+    }, [edges, id]);
+
+    return (
+      <NodeShell title={def.type + " " + id}>
+        {HeaderComponent && (
+          <HeaderComponent id={id} data={data}></HeaderComponent>
+        )}
+        {def.inputs.map((input, index) => (
+          <NodeHandleLine
+            key={input.id}
+            type="input"
+            label={input.label}
+            id={input.id}
+            connected={connections[index]}
+            input={
+              <NumberInput
+                id={input.id}
+                name={input.id}
+                label={input.label}
+                value={store[index].value}
+                onChange={(e) => store[index].setValue(e.value)}
+              ></NumberInput>
+            }
+          ></NodeHandleLine>
+        ))}
+        {def.outputs.map((output) => (
+          <NodeHandleLine
+            key={output.id}
+            id={output.id}
+            type="output"
+            label={output.label}
+          ></NodeHandleLine>
+        ))}
+      </NodeShell>
+    );
+  };
+}
--- src/nodes/AddNode.tsx
+++ src/nodes/AddNode.tsx
@@ -1,63 +1,28 @@
-import { FunctionComponent, useCallback, useEffect, useMemo } from "react";
-import { NodeProps, Handle, Position, useEdges } from "reactflow";
-import NumberInput from "../components/input/NumberInput";
-import NodeHandleLine from "../components/node/NodeHandleLine";
-import NodeShell from "../components/node/NodeShell";
-import { useInputHandleData } from "../engine/store";
+import { NodeDef } from "../engine/node";
 
-const AddNode: FunctionComponent<NodeProps<any>> = ({ data, id }) => {
-  const edges = useEdges();
-
-  const [valueA, setValueA] = useInputHandleData<number>(id, "a");
-  const [valueB, setValueB] = useInputHandleData<number>(id, "b");
-
-  const connections = useMemo(() => {
-    const inEdges = edges.filter((it) => it.target === id);
-    // const outEdges = edges.filter((it) => it.source === id);
-
-    // console.log(inEdges);
-
-    return {
-      value: inEdges.findIndex((it) => it.targetHandle === "a") >= 0,
-      value2: inEdges.findIndex((it) => it.targetHandle === "b") >= 0,
-    };
-  }, [edges, id]);
-
-  return (
-    <NodeShell title={"Add" + id}>
-      <NodeHandleLine
-        type="input"
-        label="Value"
-        id="a"
-        connected={connections["value"]}
-        input={
-          <NumberInput
-            id="a"
-            name="a"
-            label="Value"
-            value={valueA}
-            onChange={(e) => setValueA(e.value)}
-          ></NumberInput>
-        }
-      ></NodeHandleLine>
-      <NodeHandleLine
-        type="input"
-        label="Value"
-        id="b"
-        connected={connections["value2"]}
-        input={
-          <NumberInput
-            id="b"
-            name="b"
-            label="Value2"
-            value={valueB}
-            onChange={(e) => setValueB(e.value)}
-          ></NumberInput>
-        }
-      ></NodeHandleLine>
-      <NodeHandleLine id="sum" type="output" label="Value"></NodeHandleLine>
-    </NodeShell>
-  );
+export const addNodeDef: NodeDef = {
+  type: "add",
+  // component: makeNodeRenderer,
+  outputs: [
+    {
+      id: "sum",
+      label: "Sum",
+      type: "number",
+    },
+  ],
+  inputs: [
+    {
+      id: "a",
+      label: "Value",
+      type: "number",
+    },
+    {
+      id: "b",
+      label: "Value",
+      type: "number",
+    },
+  ],
+  executor: ([a, b]) => {
+    return [a + b];
+  },
 };
-
-export default AddNode;
