cfe7c1bc84001a72327807fadbaff165d5c77022
Luna The Bat <lunathebat@proton.me>
2023-07-20 08:14:44 +0200

    Add Graph Node Preview

--- src/components/node/NodeHandleValuePreview.tsx
+++ src/components/node/NodeHandleValuePreview.tsx
@@ -1,16 +1,12 @@
 import { FunctionComponent } from "react";
 import { usePreviewStore } from "../../engine/preview";
-
-const NumberPreview: FunctionComponent<NodeHandleValuePreviewProps> = (props) => {
-  const value = usePreviewStore((s) => s.outputHandlePreviews[props.nodeId]?.[props.handleId]);
-  return <div>V: {value}</div>;
-};
+import { GraphNodeValuePreview } from "./preview/GraphNodeValuePreview";
 
 export const nodeHandleValuePreviews: Record<string, FunctionComponent<NodeHandleValuePreviewProps>> = {
-  number: NumberPreview,
+  number: GraphNodeValuePreview,
 };
 
-type NodeHandleValuePreviewProps = {
+export type NodeHandleValuePreviewProps = {
   nodeId: string;
   handleId: string;
   type: string;
---
+++ src/components/node/preview/GraphNodeValuePreview.tsx
@@ -0,0 +1,100 @@
+import { FunctionComponent, useEffect, useState } from "react";
+import { usePreviewStore } from "../../../engine/preview";
+import { NodeHandleValuePreviewProps } from "../NodeHandleValuePreview";
+import { timerNodeDef } from "../../../nodes/TimerNode";
+import produce from "immer";
+
+const HISTORY_LENGTH = 3 * 60;
+const HEIGHT = 100;
+
+type GraphProps = {
+  history: (number | undefined)[];
+  index: number;
+  fixed: boolean;
+};
+const Graph: FunctionComponent<GraphProps> = ({ history, index, fixed }) => {
+  let min = 0;
+  let max = 0;
+
+  const tmpHistory = history.map((it) => {
+    const val = it || 0.0;
+    if (val < min) {
+      min = val;
+    }
+    if (val > max) {
+      max = val;
+    }
+    return val;
+  });
+
+  if (fixed) {
+    min = 0.0;
+    max = 1.0;
+  }
+
+  const offset = -(min + max) / 2;
+  const scale = HEIGHT / 2 / Math.max(0.001, Math.abs(max + offset));
+
+  const correctedHistory = tmpHistory.map((it) => (it + offset) * scale);
+
+  const parts = new Array(HISTORY_LENGTH)
+    .fill("")
+    .map((_, i) => {
+      const realIndex = (i + index) % HISTORY_LENGTH;
+      const point = correctedHistory[realIndex] || 0;
+
+      const coords = -point;
+      return `L${i} ${coords.toFixed(4)}`;
+    })
+    .join(" ");
+
+  const start = correctedHistory[index];
+
+  const zero = (0 + offset) * scale;
+  const maxLine = (max + offset) * scale;
+  const minLine = (min + offset) * scale;
+
+  return (
+    <svg viewBox={`0 ${-HEIGHT / 2} ${HISTORY_LENGTH} ${HEIGHT + 5}`} height="100" className="stroke-white fill-none">
+      <path className="stroke-slate-500" d={`M0 ${-maxLine} L${HISTORY_LENGTH} ${-maxLine}`}></path>
+      <path className="stroke-slate-500" d={`M0 ${-minLine} L${HISTORY_LENGTH} ${-minLine}`}></path>
+      <path className="stroke-slate-500" d={`M0 ${-zero} L${HISTORY_LENGTH} ${-zero}`}></path>
+      <text x="0" y={-zero} className="text-sm stroke-none fill-white">
+        {" "}
+        0
+      </text>
+      <text x="0" y={-maxLine + 10} className="text-sm stroke-none fill-white h-[10px]">
+        {max.toFixed(2)}
+      </text>
+      <text x="0" y={-minLine - 10} className="text-sm stroke-none fill-white h-[10px]">
+        {min.toFixed(2)}
+      </text>
+      <path d={`M0 ${-start} ${parts}`}></path>
+    </svg>
+  );
+};
+
+const SAMPLE_RATE = 60;
+export const GraphNodeValuePreview: FunctionComponent<NodeHandleValuePreviewProps> = (props) => {
+  const [preview, setPreview] = useState({ index: 0, history: new Array(HISTORY_LENGTH).fill(0) });
+
+  useEffect(() => {
+    let timeout = setInterval(() => {
+      const value = usePreviewStore.getState().outputHandlePreviews[props.nodeId]?.[props.handleId];
+      setPreview((s) =>
+        produce(s, (draft) => {
+          draft.history[draft.index] = value ?? null;
+          draft.index = (draft.index + 1) % HISTORY_LENGTH;
+        })
+      );
+    }, 1000 / SAMPLE_RATE);
+    return () => {
+      clearInterval(timeout);
+    };
+  }, [props.nodeId, props.handleId]);
+  return (
+    <div>
+      <Graph history={preview.history} index={preview.index} fixed={true} />
+    </div>
+  );
+};
